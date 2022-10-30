9253124e6709cd87ecc31a0da2c38549c7a29619
[carlos martinez] <theycallhellp@gmail.com>
2022-10-30 11:49:52 -0700

    create department table in schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS departments_db;
 CREATE DATABASE departments_db;
 
 USE departments_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
